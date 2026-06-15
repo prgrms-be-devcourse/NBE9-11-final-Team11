067e551ad958829e42f7e4d7808a9600651ce208
HanJeongmok <wjdahe1845@gmail.com>
2026-06-15 16:46:12 +0900

    feat: 거래 한도 검증 Validator 구현 (#4)
    * fix:소수점2자리에서 8자리로 변경
    
    * feat:송금 관련 한도 검증)
    
    * feat:한도 관련 테스트코드 작성
    
    * fix:user일일 입금 한도 검증에서 일일, 연간 테이블 분리
    
    * fix:서버 타임과 상관없이 한국 시간으로 계산하게 수정
    
    * feat:한도정책 초기데이터 data.sql에 추가
    
    * fix:날짜가져올때 서울기준으로 가져오게 수정

--- backend/src/main/java/com/fxflow/domain/userlimitusage/repository/UserLimitUsageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fxflow.domain.userlimitusage.repository;
-
-import com.fxflow.domain.userlimitusage.entity.UserLimitUsage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserLimitUsageRepository extends JpaRepository<UserLimitUsage,Long> {
-}
