56e72d4db41800bd5ecf758d2a3403b919e51a65
DavinLee <davin11992@gmail.com>
2026-06-24 02:15:49 +0900

    chore: Testcontainers 의존성 BOM 방식으로 ì

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -28,9 +28,6 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testCompileOnly("org.projectlombok:lombok")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	testImplementation("org.springframework.boot:spring-boot-testcontainers")
-	testImplementation("org.testcontainers:junit-jupiter")
-	testImplementation("org.testcontainers:postgresql")
 	testAnnotationProcessor("org.projectlombok:lombok")
 	//spring security
 	implementation("org.springframework.boot:spring-boot-starter-security")
@@ -45,6 +42,13 @@ dependencies {
 
 	//redis
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+	// Testcontainers
+	testImplementation(platform("org.testcontainers:testcontainers-bom:1.20.4"))
+	testImplementation("org.testcontainers:testcontainers")
+	testImplementation("org.testcontainers:postgresql")
+	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("org.springframework.boot:spring-boot-testcontainers") // @ServiceConnection
 }
 
 tasks.withType<Test> {
