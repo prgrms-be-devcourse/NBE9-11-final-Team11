db269993d4d36cd17c7e66c7f2ab907a85554864
DavinLee <davin11992@gmail.com>
2026-06-26 01:59:24 +0900

    refactor: SPREAD 상수를 PoolConstants로 분리

---
+++ backend/src/main/java/com/fxflow/domain/companypool/PoolConstants.java
@@ -0,0 +1,10 @@
+package com.fxflow.domain.companypool;
+
+import java.math.BigDecimal;
+
+public final class PoolConstants {
+
+    public static final BigDecimal SPREAD = new BigDecimal("0.003");
+
+    private PoolConstants() {}
+}
