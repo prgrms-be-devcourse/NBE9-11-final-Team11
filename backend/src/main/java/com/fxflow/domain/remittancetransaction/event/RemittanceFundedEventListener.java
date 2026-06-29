82a38115d5eb4c7a141488110f8e0bf91cdcbf5f
bokang <bogang12345@gmail.com>
2026-06-29 09:14:37 +0900

    카프카 세팅 취소

--- backend/src/main/java/com/fxflow/domain/remittancetransaction/event/RemittanceFundedEventListener.java
+++ backend/src/main/java/com/fxflow/domain/remittancetransaction/event/RemittanceFundedEventListener.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @RequiredArgsConstructor
 public class RemittanceFundedEventListener {
 
+
     private final RemittancePayoutService remittancePayoutService;
 
     /**
